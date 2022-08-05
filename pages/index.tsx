b4ce56aa53c5cb5ae383095d19ec9e2f66c08c8a
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-05 13:53:30 +0100

    Added socials variable and passing them into <SocialLinks>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,31 @@
 import Navbar from "../components/Navbar"
 import ProfileHeading from "../components/ProfileHeading"
+import SocialLinks from "../components/SocialLinks"
+
+const socials = [
+	{
+		name: "GitHub",
+		image: "images/githubIcon64x64.png"
+	},
+	{
+		name: "GitHub",
+		image: "images/githubIcon64x64.png"
+	},
+	{
+		name: "GitHub",
+		image: "images/githubIcon64x64.png"
+	},
+]
 
 const IndexPage = () => (
 	<div className="h-screen bg-mainBackground">
 		<Navbar />
-		<div className="flex justify-center mt-10">
+		<div className="flex items-center flex-col mt-10">
 			<ProfileHeading />
+
+			<div className="mt-10 w-6/12">
+				<SocialLinks socials={socials} />
+			</div>
 		</div>
 	</div>
 )
