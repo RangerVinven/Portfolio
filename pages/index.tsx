71f3dbc0aac7e9bbbeec5f5d01ab991b25394678
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-08-05 13:17:18 +0100

    Added the ProfileHeading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import Navbar from "../components/Navbar"
+import ProfileHeading from "../components/ProfileHeading"
 
 const IndexPage = () => (
 	<div className="h-screen bg-mainBackground">
 		<Navbar />
+		<div className="flex justify-center mt-10">
+			<ProfileHeading />
+		</div>
 	</div>
 )
 
